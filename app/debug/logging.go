61425e093af43a3c6249b482813c4ff7775318aa
Rico <rico@pgml.de>
2025-05-27 00:50:39 +0200

    forgot debug package T_T

---
+++ app/debug/logging.go
@@ -0,0 +1,64 @@
+package debug
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+const logFile = "app.log"
+
+type ErrorLvl int
+
+const (
+	Info ErrorLvl = iota
+	Debug
+	Warn
+	Error
+)
+
+var errLvl = map[ErrorLvl]string{
+	Info:  "INFO",
+	Debug: "DEBUG",
+	Warn:  "WARN",
+	Error: "ERROR",
+}
+
+func (e ErrorLvl) String() string {
+	return errLvl[e]
+}
+
+func LogInfo(args ...any) {
+	logMsg(Info, args)
+}
+
+func LogDebug(args ...any) {
+	logMsg(Debug, args)
+}
+
+func LogWarn(args ...any) {
+	logMsg(Warn, args)
+}
+
+func LogErr(args ...any) {
+	logMsg(Error, args)
+}
+
+func logMsg(level ErrorLvl, args ...any) {
+	file, err := os.OpenFile(
+		logFile,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Printf(
+		"[%s] %s: %s\n",
+		time.Now().Format("00:00:00"),
+		level.String(), args,
+	)
+}
