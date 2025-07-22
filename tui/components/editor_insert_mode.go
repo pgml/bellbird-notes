4c5b4826546046d2da71d87ed4f3211b01dbade4
Rico <rico@pgml.de>
2025-07-22 16:32:17 +0200

    added soft tab support

--- tui/components/editor_insert_mode.go
+++ tui/components/editor_insert_mode.go
@@ -1,6 +1,11 @@
 package components
 
-import tea "github.com/charmbracelet/bubbletea/v2"
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea/v2"
+	"github.com/charmbracelet/x/input"
+)
 
 func (e *Editor) handleInsertMode(msg tea.KeyMsg) tea.Cmd {
 	if msg.String() == "esc" {
@@ -13,6 +18,21 @@ func (e *Editor) handleInsertMode(msg tea.KeyMsg) tea.Cmd {
 	// only allow input when this flag is true.
 	// See tui.updateComponents() for further explanation
 	if e.CanInsert {
+		if msg.Key().Code == 9 {
+			k := msg.Key()
+			// Just for now, will be setting when tab support is finished
+			tabWidth := 4
+			// simulate soft tabs
+			tabStr := strings.Repeat(string(input.KeySpace), tabWidth)
+			msg = tea.KeyPressMsg{
+				Text:        tabStr,
+				Mod:         k.Mod,
+				Code:        k.Code,
+				ShiftedCode: k.ShiftedCode,
+				BaseCode:    k.BaseCode,
+				IsRepeat:    k.IsRepeat,
+			}
+		}
 		e.Textarea, cmd = e.Textarea.Update(msg)
 	}
 
